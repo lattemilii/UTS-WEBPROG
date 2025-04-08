31938c608218f85af8af904872a553739ad59e88
lattemilii <emilygouw4@gmail.com>
2025-04-08 15:09:03 +0700

    Update db.php

--- MyUMN(UTS)/db.php
+++ MyUMN(UTS)/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "uts_webprog");
+$con = mysqli_connect("localhost", "root", "", "myumn");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
