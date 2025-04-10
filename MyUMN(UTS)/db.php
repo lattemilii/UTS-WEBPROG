af351223d38f65b0e43b85e6c13447ed179dd211
Emily Francesca Gouw <emilygouw4@gmail.com>
2025-04-10 18:47:11 +0700

    Update db.php
    add comment

--- MyUMN(UTS)/db.php
+++ MyUMN(UTS)/db.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "uts_webprog");
-
+//if in hosting username, password, db name is different and will be attached in submission.txt
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
